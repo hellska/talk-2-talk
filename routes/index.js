5391cf7471f1cc90163ad12b32e0556c0ad760bb
Daniele Scarano <scarano.daniele@gmail.com>
2014-04-24 12:12:38 +0200

    Module User Management: registration form and login skeletons added

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'talk 2 Talk' });
 });
 
-/* GET home page. */
+/* GET users list. */
 router.get('/mydb', function(req, res){
   var db = req.db;
   var collection = db.get('users');
@@ -15,6 +15,66 @@ router.get('/mydb', function(req, res){
       "mydb" : docs
     });
   });
+});
+
+router.get('/register', function(req, res) {
+  res.render('register', { title: 'Registration Form  ' });
+});
+
+// adding new users
+router.post('/register', function(req, res){
+  var db = req.db;
+  
+  var name = req.body.name;
+  var lastname = req.body.lastname;
+  var email = req.body.email;
+  var username = req.body.username;
+  var passwd = req.body.pass;
+  
+  var collection = db.get('users');
+  
+  collection.insert({
+    "name" : name,
+    "lastname" : lastname,
+    "email" : email,
+    "username" : username,
+    "passwd" : passwd
+  }, function(err, doc) {
+    if(err){
+      res.send;
+    } else {
+      res.location("mydb");
+      res.redirect("mydb");
+    }
+  });
+});
+
+router.post('/login', function(req, res){
+  var db = req.db;
+  
+  var user = req.body.user;
+  var passwd = req.body.passwd;
+  
+  console.log("username: " + user + " pwd: " + passwd)
+  
+  var collection = db.get('users');
+  
+  var query = {};
+  query['username'] = user;
+  
+  collection.findOne( {'username' : user, 'passwd' : passwd }, function(err, doc) {
+    console.log(err);
+    console.log(doc);
+    if (doc === null ) {
+      console.log("No such a user")
+      res.location("/");
+      res.redirect("/");
+    } else {
+      res.location("mydb");
+      res.redirect("mydb");
+    }
+  });
+  
 });
 
 module.exports = router;
